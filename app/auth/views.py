8a693ee6f8d83bf55f042fbdec07dda8bde07c7e
Abirdcfly <fp544037857@gmail.com>
2016-02-28 15:34:42 +0800

    register page 90

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,8 @@ from flask import render_template, redirect, request, url_for, flash
 from flask.ext.login import login_user, login_required, logout_user
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -25,3 +26,14 @@ def logout():
     logout_user()
     flash(u'你已退出登录！')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.username.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        flash(u'你已注册成功！')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
