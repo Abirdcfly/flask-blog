98db82c9cfc3a18fe631d1f281a5d34f869a0d7f
Abirdcfly <fp544037857@gmail.com>
2016-03-03 18:03:23 +0800

    SAE EMAIL-test-sina2

--- app/main/views.py
+++ app/main/views.py
@@ -22,28 +22,28 @@ def index():
     #     db.create_all()
     # except:
     #     pass
-    # title = 'Home'
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if current_app.config['WOTER_ADMIN']:
-                send_email(current_app.config['WOTER_ADMIN'], U'新用户加入',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        if session['known'] == False:
-            flash(u'欢迎新成员')
-        else:
-            flash(u'欢迎回来')
-        form.name.data = ''
-        return redirect(url_for('.index'))
-    return render_template('index.html', title=title, form=form, name=session.get('name'),
-                           known=session.get('known', False))
+    title = 'Home'
+    # form = NameForm()
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(username=form.name.data).first()
+    #     if user is None:
+    #         user = User(username=form.name.data)
+    #         db.session.add(user)
+    #         session['known'] = False
+    #         if current_app.config['WOTER_ADMIN']:
+    #             send_email(current_app.config['WOTER_ADMIN'], U'新用户加入',
+    #                        'mail/new_user', user=user)
+    #     else:
+    #         session['known'] = True
+    #     session['name'] = form.name.data
+    #     if session['known'] == False:
+    #         flash(u'欢迎新成员')
+    #     else:
+    #         flash(u'欢迎回来')
+    #     form.name.data = ''
+    #     return redirect(url_for('.index'))
+    return render_template('index.html', title=title)
+    # , form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @main.route('/user/<username>')
