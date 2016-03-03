ff0cef17a6ae9d0be4874d6c8362bde2ea2b1a37
Abirdcfly <fp544037857@gmail.com>
2016-03-03 16:36:56 +0800

    SAE SQLfinish

--- app/auth/views.py
+++ app/auth/views.py
@@ -34,6 +34,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        flash(u"收到数据")
         db.session.add(user)
         db.session.commit()
         token = user.generate_confirmation_token()
