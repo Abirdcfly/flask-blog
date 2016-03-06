150b4473addb0c2818dadba7977a20d9ef63d1b9
Abirdcfly <fp544037857@gmail.com>
2016-03-06 17:48:10 +0800

    SAE user avatar finish

--- app/main/views.py
+++ app/main/views.py
@@ -403,6 +403,5 @@ def upload_avatar():
             db.session.add(current_user)
             db.session.commit()
             flash(u"头像已修改！")
-            return str(current_user.gravatar)
-            # return redirect(url_for('main.user_page', username=current_user.username))
+            return redirect(url_for('main.user_page', username=current_user.username))
     return render_template('upload_avatar.html', user=current_user)
