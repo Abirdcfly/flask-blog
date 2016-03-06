8ae4164875b70098ce453597729472c46815d946
Abirdcfly <fp544037857@gmail.com>
2016-03-06 17:44:08 +0800

    SAE user model change avatar_hash

--- app/models.py
+++ app/models.py
@@ -143,7 +143,7 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
-     @property
+    @property
     def gravatar(self,  size=100, default='identicon', rating='g'):
         if self.avatar_local_url != None:
             return self.avatar_local_url
