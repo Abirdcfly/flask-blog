5699ae2cf6d72a8e173586167df88b13793a513a
Abirdcfly <fp544037857@gmail.com>
2016-03-06 15:16:08 +0800

    SAE upload test

--- app/main/views.py
+++ app/main/views.py
@@ -386,8 +386,19 @@ def allowed_file(filename):
 
 
 # 存储头像到SAE storage
-from sae.storage import Bucket
-bucket = Bucket('avatar')
+# from sae.storage import Bucket
+# # s = sae.storage.Client()
+# bucket = Bucket('avatar')
+# current_app.config['ACCESS_KEY']=
+
+
+import sae.const
+    access_key = sae.const.ACCESS_KEY
+    secret_key = sae.const.SECRET_KEY
+    appname = sae.const.APP_NAME
+    domain_name = "avatar"  #刚申请的domain
+
+import sae.storage
 
 
 @main.route('/upload', methods=['GET', 'POST'])
@@ -395,6 +406,11 @@ def upload_avatar():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
-            bucket.put_object(file, open(__file__, 'rb'))
-            return redirect(url_for('main.user_page', username=current_user.username))
+            s = sae.storage.Client()
+            ob = sae.storage.Object(file.read())
+            url = s.put(domain_name, file.filename, ob)
+            # bucket.put_object('1.txt', 'hello, world')
+            # bucket.put_object(file, open(__file__, 'rb'))
+            # return redirect(url_for('main.user_page', username=current_user.username))
+            return redirect(url)
     return render_template('upload_avatar.html', user=current_user)
